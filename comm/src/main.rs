4c49c7ec0556a8df6f2ebae2120872a67b7a425f
paivan1999 <paivan1999@mail.ru>
2022-09-22 22:31:28 +0300

    mistake in comm

--- comm/src/main.rs
+++ comm/src/main.rs
@@ -1,5 +1,30 @@
 #![forbid(unsafe_code)]
 
+use std::{
+    collections::HashSet,
+    env,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 fn main() {
-    // TODO: your code here.
+    let args = env::args().collect::<Vec<String>>();
+    let mut file1_strings = HashSet::new();
+    let mut final_strings = HashSet::new();
+    let file1 = File::open(&args[0]).unwrap();
+    let file2 = File::open(&args[1]).unwrap();
+    let reader = BufReader::new(file1);
+    for line in reader.lines() {
+        file1_strings.insert(line.unwrap());
+    }
+    let reader = BufReader::new(file2);
+    for line in reader.lines() {
+        let str_from_line = line.unwrap();
+        if file1_strings.contains(&str_from_line) {
+            if !final_strings.contains(&str_from_line) {
+                println!("{}", str_from_line);
+                final_strings.insert(str_from_line);
+            }
+        }
+    }
 }
